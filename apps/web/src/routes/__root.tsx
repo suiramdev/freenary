95338b50aefc502704e7acaaa032066f66b6fdf9
suiramdev <me@suiram.dev>
2026-08-31 11:41:14 +0200

    Rename app title from 'My App' to 'Freenary'

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -50,7 +50,7 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "My App",
+        title: "Freenary",
       },
     ],
     links: [
